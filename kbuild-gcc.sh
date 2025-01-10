0fc59385da09b2e3df183aec5b602bced19aea88
ruyue <ruyueee@outlook.com>
2025-01-10 19:15:35 +0800

    gcc-wrapper:rewrited by py3

--- kbuild-gcc.sh
+++ kbuild-gcc.sh
@@ -3,21 +3,17 @@
 export ARCH=arm64
 export SUBARCH=arm64
 
-CLANG_PATH=$(pwd)/prebuilts/clang/host/linux-x86/clang-r416183b/bin/clang
+#CLANG_PATH=$(pwd)/prebuilts/clang/host/linux-x86/clang-r416183b/bin/clang
 CROSS_COMPILE_PATH=$(pwd)/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
 CROSS_COMPILE_ARM32_PATH=$(pwd)/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
 
 
 make ARCH=arm64 \
-    CC=$CLANG_PATH \
-    CLANG_TRIPLE=aarch64-linux-gnu- \
     CROSS_COMPILE=$CROSS_COMPILE_PATH \
-    Judau_defconfig
+    pucci_defconfig
 
 
 make -j20 ARCH=arm64 \
-    CC=$CLANG_PATH \
-    CLANG_TRIPLE=aarch64-linux-gnu- \
     CROSS_COMPILE=$CROSS_COMPILE_PATH \
     CROSS_COMPILE_ARM32=$CROSS_COMPILE_ARM32_PATH \
     CFLAGS="-I$(pwd)/drivers/pinctrl -Wno-format -Wno-error"
